e777fe242b48637a68947911c3d7f7e6ee0fbe85
ArFn-18 <husniarifin18@gmail.com>
2024-06-12 11:13:22 +0700

    change title-bar

--- main.php
+++ main.php
@@ -11,7 +11,7 @@ if (empty($_SESSION['username_thecoffe'])) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>FollowUp | <?php echo (ucfirst($_GET['page'])); ?></title>
+    <title>TheCoffe | <?php echo (ucfirst($_GET['page'])); ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="src/css/style.css">
